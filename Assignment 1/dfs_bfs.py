6755a3bc6e3122be6d17df178f6e080f00de9909
Paarth Kothari <paarthkothari14@gmail.com>
2022-05-21 17:24:37 +0530

    first commit

---
+++ Assignment 1/dfs_bfs.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def DFSUtil(self, v, visited):
+        visited.add(v)
+        print(v, end=' ')
+
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    def DFS(self, v):
+        visited = set()
+        self.DFSUtil(v, visited)
+
+    def BFS(self, s):
+        visited = [False] * (max(self.graph) + 1)
+        queue = []
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(0, 3)
+g.addEdge(1, 0)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 1)
+g.addEdge(2, 4)
+g.addEdge(3, 0)
+g.addEdge(4, 2)
+
+
+print("Following is DFS from (starting from vertex 0)")
+g.DFS(0)
+
+
+print("\n Following is BFS from (starting from vertex 2)")
+g.BFS(2)
---
+++ Assignment 3/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+
+
+import sys
+A = [64, 25, 12, 22, 11]
+
+# Traverse through all array elements
