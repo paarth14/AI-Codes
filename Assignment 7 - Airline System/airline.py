e67fdfad30288a3544eefeacb802c98f697b71c9
Paarth Kothari <paarthkothari14@gmail.com>
2022-05-22 13:42:48 +0530

    Added Airline System

---
+++ Assignment 7 - Airline System/airline.py
@@ -0,0 +1,32 @@
+# pip install experta
+from experta import *
+from random import randint
+# from collections.abc import Mapping
+# from collections import Mapping
+
+
+class windSpeed(Fact):
+    """ info of wind speed"""
+    pass
+
+
+class ATS(KnowledgeEngine):
+
+    @Rule(windSpeed(P(lambda x: x < 5)))
+    def perfect(self):
+
+        print("Perfect wind speed to land go ahead !")
+
+    @Rule(windSpeed(P(lambda x: x >= 5) & P(lambda x: x <= 10)))
+    def caution(self):
+        print("Wind speed is bit high. Proceed with caution")
+
+    @Rule(windSpeed(P(lambda x: x > 10)))
+    def dont(self):
+        print("Wind Speed to high do not try to land.. Crash imminent ")
+
+
+engine = ATS()
+engine.reset()
+engine.declare(windSpeed(randint(0, 12)))
+engine.run()
